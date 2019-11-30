4845564be881f66070c1165f8f374fa11c272b6f
Tanvir Islam streame <streametanvir@gmail.com>
2019-11-30 23:19:26 +0600

    Added update and delete functionality

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -1,16 +1,16 @@
 require("../models/food.model")
-const  express = require("express");
+const express = require("express");
 const router = express.Router();
 const mongoose = require('mongoose');
 const Food = mongoose.model('Food');
 
 
-router.get("/food-list/", (req ,res)=> {
+router.get("/food-list/", (req, res) => {
     Food.find({})
-        .then(data=>{
+        .then(data => {
             res.status(200).json({
                 count: data.length,
-                food: data.map(data=> {
+                food: data.map(data => {
                     return {
                         id: data.id,
                         name: data.name,
@@ -21,15 +21,50 @@ router.get("/food-list/", (req ,res)=> {
         })
 });
 
-router.post("/food-add/", (req ,res)=> {
-    var food = new Food();
-    food.name = req.body.name;
-    food.price = req.body.price;
-    food.save((err, doc)=> {
-        if(!err) console.log("Food added succesfully");
-        else console.log("Error",err);
+router.post("/food-add/", (req, res) => {
+    var food = new Food(req.body);
+    food.save((err, doc) => {
+        if (!err) console.log("Food added succesfully");
+        else console.log("Error", err);
     })
     res.json(food);
+});
+
+router.patch("/food-update/", (req, res) => {
+    Food.findByIdAndUpdate(
+        req.query.Id,
+        {
+            $set:{
+                ...req.body
+            }
+        },
+        {
+            useFindAndModify: false
+        },
+        (err, food) => {
+
+            if (err) return res.status(500).json(err);
+            return res.json({
+                id: food._id,
+                message: "Food updated succuessfully"
+            });
+        }
+    )
+});
+
+router.delete("/food-delete/", (req, res) => {
+
+    Food.findByIdAndDelete(req.query.Id, (err, food) => {
+        if (!food) return res.status(500).send({
+            error: "Food not found"
+        });
+
+        return res.status(200).send({
+            message: "Food successfully deleted",
+            id: food._id
+        });
+    });
+
 });
 
 module.exports = router;
